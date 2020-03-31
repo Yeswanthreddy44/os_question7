743d2bf470b4398ea714714641f887661ea0780a
Yeswanthreddy44 <62049955+Yeswanthreddy44@users.noreply.github.com>
2020-03-31 19:38:24 +0530

    Question7

--- question_7.cpp
+++ question_7.cpp
@@ -2,94 +2,110 @@
 
  struct job{
  int pid,at,bt,cmpt,rbt;
-}
+ }
 f[100], s[100], m[100];
 int n, fc=0, sc=0, mc=0;
 int Quanta;
 
 void roundRobin(){
-	int time= m[0].at, mark=0, cc=0, i, rc;
-	while(time!=120 && cc!=mc){
-		for(i=0; i<=mark; i++){
-			if(m[i].rbt > Quanta){
-				time += Quanta;
-				m[i].rbt -= Quanta;
-			}
-			else if(m[i].rbt <=Quanta && m[i].rbt !=0){
-				time += m[i].rbt;
-				m[i].rbt =0;
-				m[i].cmpt = time;
-				cc++;
-			}
-			else;
-		}
-		int start = mark+1;
-		for(rc= start; rc<mc; rc++){
-			if(m[rc].at <= time){
-				mark++;
-			}
-		}
-	}	
+int time= m[0].at, mark=0, cc=0, i, rc;
+while(time!=120 && cc!=mc)
+{
+for(i=0; i<=mark; i++)
+{
+if(m[i].rbt > Quanta)
+{
+time += Quanta;
+m[i].rbt -= Quanta;
+}
+else if(m[i].rbt <=Quanta && m[i].rbt !=0)
+{
+time += m[i].rbt;
+m[i].rbt =0;
+m[i].cmpt = time;
+cc++;
+}
+else;
+}
+int start = mark+1;
+for(rc= start; rc<mc; rc++)
+{
+if(m[rc].at <= time)
+{
+mark++;
+}
+}
+}	
 }
 
-void merger(){
-	int isc=0, ifc= 0, min, flag;
-	if( fc!=0  && sc!=0){
-		while(isc<sc && ifc<fc){
-			if(f[ifc].at == s[isc].at){
-				m[mc] = f[ifc];
-				mc++;
-				ifc++;
-				m[mc]= s[isc];
-				mc++;
-				isc++;
-			}
-			else if(f[ifc].at < s[isc].at){
-				m[mc]= f[ifc];
-				mc++;
-				ifc++;
-			}
-			else if(f[ifc].at > s[isc].at){
-				m[mc]= s[isc];
-				mc++;
-				isc++;
-			}
-			else;
-		}
-		if(mc != (fc+sc)){
-			if(fc!=ifc){
-				while(ifc!=fc){
-					m[mc]= f[ifc];
-					mc=+1;
-					ifc++;
-				}
-			}
-			else if(sc!=isc){
-				while(isc!=sc){
-					m[mc]= s[isc];
-					mc++;
-					isc++;
-				}
-			}
-		}
-	}
-	else if(fc==0){
-		while(isc!=sc){
-			m[mc]= s[isc];
-			mc++;
-			isc++;
-		}
-	}
-	else if(sc==0){
-		while(ifc!=fc){
-			m[mc]= f[ifc];
-			mc++;
-			ifc++;
-		}
-	}
-	else {
-		printf("\n No valid Jobs available\n");
-	}
+void merger()
+{
+int isc=0, ifc= 0, min, flag;
+if( fc!=0  && sc!=0)
+{
+while(isc<sc && ifc<fc)
+{
+if(f[ifc].at == s[isc].at)
+{
+m[mc] = f[ifc];
+mc++;
+ifc++;
+m[mc]= s[isc];
+mc++;
+isc++;
+}
+else if(f[ifc].at < s[isc].at)
+{
+ m[mc]= f[ifc];
+ mc++;
+ifc++;
+}
+else if(f[ifc].at > s[isc].at)
+mc++;
+isc++;
+}
+else;
+}
+if(mc != (fc+sc))
+{
+if(fc!=ifc)
+{
+while(ifc!=fc)
+{
+ m[mc]= f[ifc];
+ mc=+1;
+ ifc++;
+}
+}
+else if(sc!=isc)
+{
+while(isc!=sc){
+m[mc]= s[isc];
+mc++;
+isc++;
+}
+}
+}
+}
+else if(fc==0)
+{
+while(isc!=sc){
+m[mc]= s[isc];
+mc++;
+isc++;
+}
+}
+else if(sc==0)
+{
+while(ifc!=fc){
+m[mc]= f[ifc];
+mc++;
+ifc++;
+}
+}
+else {
+ printf("\n No valid Jobs available\n");
+}
 }
 
 void printer(){
@@ -97,7 +113,8 @@ void printer(){
 	double avg;
 	printf("\nSummary for the Execution\n");
 	printf("\nQuery ID\tArrival Time\tRessolving Time\tCompletion Time\tTurn Around Time\tWaiting Time");
-	for(i; i<mc; i++){
+	for(i; i<mc; i++)
+	{
 		printf("\n%d\t\t%d\t\t%d\t\t%d\t\t%d\t\t\t%d",
 		m[i].pid, (m[i].at+1000), m[i].bt, (m[i].cmpt+1000), (m[i].cmpt-m[i].at), ((m[i].cmpt-m[i].at)- m[i].bt));
 		total= m[i].cmpt;
